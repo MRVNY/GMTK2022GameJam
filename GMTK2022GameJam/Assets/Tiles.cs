0bad0c045b14489417f112b514dd9fc729e43820
jdk-eco <100361256+jdk-eco@users.noreply.github.com>
2022-07-16 20:56:28 +0200

    Changing upper and lower to curr and goal in Tile.cs

--- GMTK2022GameJam/Assets/Tiles.cs
+++ GMTK2022GameJam/Assets/Tiles.cs
@@ -6,8 +6,8 @@ using UnityEngine.Tilemaps;
 
 public class Tiles : MonoBehaviour
 {
-    public static Tilemap lower;
-    public static Tilemap upper;
+    public static Tilemap current;
+    public static Tilemap goal;
     public Dice dice;
 
     public static List<Vector3> availablePlaces;
@@ -17,20 +17,20 @@ public class Tiles : MonoBehaviour
     void Start()
     {
         var tileMaps = GetComponentsInChildren<Tilemap>();
-        upper = tileMaps[0];
-        lower = tileMaps[1];
-        Assert.IsTrue(upper.gameObject.name.Equals("Upper"));
+        goal = tileMaps[0];
+        current = tileMaps[1];
+        Assert.IsTrue(goal.gameObject.name.Equals("Goal"));
 
         availablePlaces = new List<Vector3>();
  
-        for (int n = lower.cellBounds.xMin; n < lower.cellBounds.xMax; n++)
+        for (int n = current.cellBounds.xMin; n < current.cellBounds.xMax; n++)
         {
-            for (int p = lower.cellBounds.yMin; p < lower.cellBounds.yMax; p++)
+            for (int p = current.cellBounds.yMin; p < current.cellBounds.yMax; p++)
             {
-                Vector3Int localPlace = (new Vector3Int(n, p, (int)lower.transform.position.y));
-                lower.SetTileFlags(localPlace, TileFlags.None);
-                Vector3 place = lower.CellToWorld(localPlace);
-                if (lower.HasTile(localPlace))
+                Vector3Int localPlace = (new Vector3Int(n, p, (int)current.transform.position.y));
+                current.SetTileFlags(localPlace, TileFlags.None);
+                Vector3 place = current.CellToWorld(localPlace);
+                if (current.HasTile(localPlace))
                 {
                     //Tile at "place"
                     availablePlaces.Add(place);
@@ -48,16 +48,16 @@ public class Tiles : MonoBehaviour
         Vector3 offset = new Vector3(0.7f, 0, 0.7f);
         if (Dice.downFace != null)
         {
-            var tilePos = lower.WorldToCell(Dice.downFace.transform.position);
-            var previousColor = lower.GetColor(tilePos);
-            var goalColor = upper.GetColor(tilePos);
+            var tilePos = current.WorldToCell(Dice.downFace.transform.position);
+            var previousColor = current.GetColor(tilePos);
+            var goalColor = goal.GetColor(tilePos);
 
             if (Color.Equals(previousColor, Dice.downFace.color)) // nothing has changed
             {
                 return;
             }
 
-            lower.SetColor(tilePos, Dice.downFace.color);
+            current.SetColor(tilePos, Dice.downFace.color);
 
             if (Color.Equals(Dice.downFace.color, goalColor)) // changed to good Color
             {
