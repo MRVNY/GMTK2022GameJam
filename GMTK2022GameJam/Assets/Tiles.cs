95f68aab701bfc85bef064aaa708e9928d8fd9f3
jdk-eco <100361256+jdk-eco@users.noreply.github.com>
2022-07-16 19:19:47 +0200

    Check if game is over.

--- GMTK2022GameJam/Assets/Tiles.cs
+++ GMTK2022GameJam/Assets/Tiles.cs
@@ -1,29 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 using UnityEngine.Tilemaps;
 
 public class Tiles : MonoBehaviour
 {
-    public static Tilemap tileMap;
+    public static Tilemap lower;
+    public static Tilemap upper;
     public Dice dice;
 
     public static List<Vector3> availablePlaces;
+
+    private static int validTiles = 0;
     // Start is called before the first frame update
     void Start()
     {
-        tileMap = GetComponent<Tilemap>();
-        
+        var tileMaps = GetComponentsInChildren<Tilemap>();
+        upper = tileMaps[0];
+        lower = tileMaps[1];
+        Assert.IsTrue(upper.gameObject.name.Equals("Upper"));
+
         availablePlaces = new List<Vector3>();
  
-        for (int n = tileMap.cellBounds.xMin; n < tileMap.cellBounds.xMax; n++)
+        for (int n = lower.cellBounds.xMin; n < lower.cellBounds.xMax; n++)
         {
-            for (int p = tileMap.cellBounds.yMin; p < tileMap.cellBounds.yMax; p++)
+            for (int p = lower.cellBounds.yMin; p < lower.cellBounds.yMax; p++)
             {
-                Vector3Int localPlace = (new Vector3Int(n, p, (int)tileMap.transform.position.y));
-                tileMap.SetTileFlags(localPlace, TileFlags.None);
-                Vector3 place = tileMap.CellToWorld(localPlace);
-                if (tileMap.HasTile(localPlace))
+                Vector3Int localPlace = (new Vector3Int(n, p, (int)lower.transform.position.y));
+                lower.SetTileFlags(localPlace, TileFlags.None);
+                Vector3 place = lower.CellToWorld(localPlace);
+                if (lower.HasTile(localPlace))
                 {
                     //Tile at "place"
                     availablePlaces.Add(place);
@@ -41,7 +48,24 @@ public class Tiles : MonoBehaviour
         Vector3 offset = new Vector3(0.7f, 0, 0.7f);
         if (Dice.downFace != null)
         {
-            tileMap.SetColor(tileMap.WorldToCell(Dice.downFace.transform.position-offset), Dice.downFace.color);
+            var tilePos = lower.WorldToCell(Dice.downFace.transform.position - offset);
+            var previousColor = lower.GetColor(tilePos);
+            var goalColor = upper.GetColor(tilePos);
+            lower.SetColor(tilePos, Dice.downFace.color);
+            if(Color.Equals(previousColor, Dice.downFace.color)) // nothing has changed
+            {
+                return;
+            }
+            else if (Color.Equals(Dice.downFace.color, goalColor)) // changed to good Color
+            {
+                validTiles++;
+            }
+            else //the Color has changed from goal Color to a wrong Color
+            {
+                validTiles--;
+            }
+            Debug.Log(validTiles);
         }
+
     }
 }
