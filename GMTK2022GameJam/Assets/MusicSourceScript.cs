8bc9e34eea9bb9e2978d2e2cea9b6c38439d9d86
DamienMarillet <damien.marillet.dm@gmail.com>
2022-07-17 20:21:07 +0200

    Last level loop to menu scene

--- GMTK2022GameJam/Assets/MusicSourceScript.cs
+++ GMTK2022GameJam/Assets/MusicSourceScript.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class MusicSourceScript : MonoBehaviour
 {
+    public static MusicSourceScript Instance { get; private set; }
     private AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
+        if (Instance==null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            return;
+            //throw new InvalidImplementationException("You should not try to instantiate a singleton twice !");
+        }
         audioSource = GetComponent<AudioSource>();
         //audioSource.Play();
         DontDestroyOnLoad(this);
