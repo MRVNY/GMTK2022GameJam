2d01490f23615c4210335d7761cf47bd721bdf60
Yapichol <yapichol.karlcleuziou@gmail.com>
2024-04-05 21:59:34 +0200

    Moving Instantiate3dFont to Editor File to fix build

--- GMTK2022GameJam/Assets/Scripts/Editor/Instantiate3dFont.cs
+++ GMTK2022GameJam/Assets/Scripts/Editor/Instantiate3dFont.cs
@@ -1,7 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 using static UnityEngine.GraphicsBuffer;
 
 [CustomEditor(typeof(LevelSelectTrigger))]
