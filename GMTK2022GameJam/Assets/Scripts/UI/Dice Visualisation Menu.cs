63d0d9a30980df8faa6ed98612e1a0bc802c7e40
Damien <48656735+Baloouis@users.noreply.github.com>
2024-04-05 16:49:37 +0200

    feat: update dice visualisation ui messages

--- GMTK2022GameJam/Assets/Scripts/UI/Dice Visualisation Menu.cs
+++ GMTK2022GameJam/Assets/Scripts/UI/Dice Visualisation Menu.cs
@@ -8,6 +8,13 @@ public class DiceVisualisationMenu : MonoBehaviour
     [SerializeField]
     private GameObject panel;
 
+    [SerializeField]
+    private GameObject newDicePanel;
+
+
+    [SerializeField]
+    private bool isNewDicePanelVisible;
+    
     private const int _pausePriorityValue = 0;
     private bool _fadeInStarted;
 
@@ -15,13 +22,16 @@ public class DiceVisualisationMenu : MonoBehaviour
     void Start()
     {
         PauseManager.Instance.SetGameInPause(true, 1);
+
+        newDicePanel.SetActive(isNewDicePanelVisible);
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return))
+        //Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return)
+        
+        if (Input.anyKeyDown && !Input.GetMouseButtonDown(0) && !Input.GetMouseButtonDown(1) && !Input.GetMouseButtonDown(2))
         {
             PauseManager.Instance.SetGameInPause(false, 1);
             panel.SetActive(false);
